b09d93e1a608b02e181cd7329d344363bcfaf0e4
oshainhere <oshahere46@gmail.com>
2026-06-07 05:56:55 +0700

    Fix Headers error on Vercel by disabling persistSession

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,12 @@ import { motion } from 'framer-motion';
 
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL || '',
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ''
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '',
+  {
+    auth: {
+      persistSession: false,
+    },
+  }
 );
 
 export default function Home() {
